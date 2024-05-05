5bb759c9d855f8384b9fc70464cc92f32f087472
Indeok Seo <122246882+sidsidsidsids@users.noreply.github.com>
2024-05-05 10:17:53 +0900

    [Silver I] Title: IOIOI, Time: 852 ms, Memory: 73608 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -3,12 +3,31 @@ m = int(input())
 s = input()
 
 target = 'I' + ('OI'*n)
+len_t = len(target)
+len_s = len(s)
 
-cnt = 0
+table = [0] * len_t
+pidx = 0
+for idx in range(1, len_t):
+    while pidx and target[pidx] != target[idx]:
+        pidx = table[pidx - 1]
 
-for i in range(m-n+1):
-    check = s[i:i+(n*2)+1]
-    if check == target:
-        cnt += 1
+    if target[idx] == target[pidx]:
+        pidx += 1
+        table[idx] = pidx
 
-print(cnt)
+answer = 0
+pidx = 0
+
+for idx in range(len_s):
+    while pidx > 0 and s[idx] != target[pidx]:
+        pidx = table[pidx - 1]
+
+    if s[idx] == target[pidx]:
+        if pidx == len_t - 1:
+            answer += 1
+            pidx = table[pidx]
+        else:
+            pidx += 1
+
+print(answer)
