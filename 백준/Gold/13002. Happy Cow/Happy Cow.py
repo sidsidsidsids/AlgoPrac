669503ea4a2062cd790005b4f63072cc6ff46cfb
Indeok Seo <122246882+sidsidsidsids@users.noreply.github.com>
2024-09-05 10:51:10 +0900

    [Gold III] Title: Happy Cow, Time: 3168 ms, Memory: 140948 KB -BaekjoonHub

---
+++ 백준/Gold/13002. Happy Cow/Happy Cow.py
@@ -0,0 +1,17 @@
+import sys
+sys.setrecursionlimit(2000^2 + 1)
+
+N = int(sys.stdin.readline().rstrip('\n'))
+H = list(map(int, sys.stdin.readline().rstrip('\n').split()))
+
+dp = [[0] * N for _ in range(N)]
+
+def func(l: int, r: int, depth: int):
+    if l > r:
+        return 0
+    if dp[l][r]:
+        return dp[l][r]
+    dp[l][r] = max(func(l + 1, r, depth + 1) + H[l] * depth, func(l, r - 1, depth + 1) + H[r] * depth)
+    return dp[l][r]
+
+print(func(0, N-1, 1))
