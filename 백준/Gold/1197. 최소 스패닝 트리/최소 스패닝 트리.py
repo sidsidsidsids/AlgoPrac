531863b9a686d32248fa915d1f54248135baa108
Indeok Seo <122246882+sidsidsidsids@users.noreply.github.com>
2025-01-20 10:57:04 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 500 ms, Memory: 66532 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -2,26 +2,22 @@ import heapq
 import sys
 
 V, E = map(int,sys.stdin.readline().rstrip("\n").split())
-graph = dict()
+graph = [[] for _ in range(V+1)]
 for _ in range(E):
     s, e, v = map(int,sys.stdin.readline().rstrip("\n").split())
-    if s not in graph:
-        graph[s] = dict()
-    if e not in graph:
-        graph[e] = dict()
-    graph[s][e] = v
-    graph[e][s] = v
+    graph[s].append((e, v))
+    graph[e].append((s, v))
 
 hq = [(0, 1)]
-V = [0] * (V+1)
+visit = [0] * (V+1)
 answer = 0
 while hq:
     weight, pos = heapq.heappop(hq)
-    if not V[pos]:
-        V[pos] = 1
+    if not visit[pos]:
+        visit[pos] = 1
         answer += weight
-        for key, val in graph[pos].items():
-            if not V[key]:
-                heapq.heappush(hq, (val, key))
+        for node, cost in graph[pos]:
+            if not visit[node]:
+                heapq.heappush(hq, (cost, node))
 
 print(answer)
