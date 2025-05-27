974850175e92f909aa22b88c8c7dc9c7f3138521
Indeok Seo <122246882+sidsidsidsids@users.noreply.github.com>
2025-05-27 21:15:31 +0900

    [Bronze V] Title: A+B - 7, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -0,0 +1,10 @@
+n = 1
+for _ in range(int(input())):
+    print(f"Case #{n}: {sum(list(map(int, input().split())))}")
+    n += 1
+    
+#Case #1: 2
+#Case #2: 5
+#Case #3: 7
+#Case #4: 17
+#Case #5: 7
